b788041b29be9fa1de845eddf3613179dfd30c89
TrekHub <teddyomondi001@gmail.com>
2021-05-18 17:31:29 +0300

    Added the header and the input component

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+<header class="text-center bg-indigo-700 p-4  font-white rounded m-3">
+    <div class="text-3xlmd:text-5xl font-bold ">Todo App</div>
+
+</header>
+ 
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/InputBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-row flex-wrap align-center justify-center text-center">
+        <input type="text" placeholder="Enter Todo" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative w-48 m-10 bg-white bg-white rounded text-sm border border-indigo-500 outline-none focus:outline-none focus:ring w-full"/>
+        <button  class="bg-pink-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">Create</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'InputBox'
+}
+</script>
